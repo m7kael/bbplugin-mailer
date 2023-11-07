973ebc4a56dca31de2ff82c8c5038cc673b2244f
Mikael Huldin <112936118+m7kael@users.noreply.github.com>
2023-11-07 20:49:18 +0100

    First commit

---
+++ src/automation.ts
@@ -0,0 +1,39 @@
+import { AutomationStepInput } from "@budibase/types"
+import nodemailer from 'nodemailer'
+
+export default async function run({ inputs }: AutomationStepInput) {
+
+  const transporter = nodemailer.createTransport({
+    host: inputs.host,
+    port: inputs.port,
+    secure: true,
+    auth: {
+      user: inputs.user,
+      pass: inputs.pass
+    }
+  })
+
+  const options = {
+    from: inputs.emailFrom,
+    to: inputs.emailTo,
+    subject: inputs.subject,
+    html: inputs.html
+  }
+  
+  try {
+    const info = await transporter.sendMail(options);
+    const message = JSON.stringify(info);
+    console.log(message);
+    return {
+      success: true,
+      message,
+    };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return {
+      success: false,
+      message: 'Error sending email: ' + error,
+    };
+  }
+  
+}
